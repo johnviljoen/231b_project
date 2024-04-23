06fcad1fec10dc24099aece132d033d02c60ac88
yutaka <purewater0901@gmail.com>
2024-04-23 03:25:16 -0700

    update
    Signed-off-by: yutaka <purewater0901@gmail.com>

--- code/UKF/estRun.py
+++ code/UKF/estRun.py
@@ -70,22 +70,39 @@ def estRun(time, dt, internalStateIn, steeringAngle, pedalSpeed, measurement):
     count_ww = internalStateIn[7]
 
     # covariances found from statistical analysis - hardcoded here
-    Sigma_vv = np.array([
-        [ 0.20586454,  0.02667422, -0.00290327],
-        [ 0.02667422,  0.2355549 ,  0.00036677],
-        [-0.00290327,  0.00036677,  0.13376413]
-    ])
+    #Sigma_vv = np.array([
+    #    [ 0.20586454,  0.02667422, -0.00290327],
+    #    [ 0.02667422,  0.2355549 ,  0.00036677],
+    #    [-0.00290327,  0.00036677,  0.13376413]
+    #])
+    B = 0.8
+    Sigma_vv = np.zeros((3, 3))
+    Sigma_vv[0,0] = 25 * um[0] * um[0] * dt * dt * np.cos(xm[2]) * np.cos(xm[2]) * 0.0002 + 0.01
+    Sigma_vv[1,1] = 25 * um[0] * um[0] * dt * dt * np.sin(xm[2]) * np.sin(xm[2]) * 0.0002 + 0.01
+    Sigma_vv[2,2] = 25 * um[0] * um[0] * dt * dt * 0.0002 / 0.2133 * np.tan(um[1]) * np.tan(um[1]) + 0.01
+
+    Sigma_vv[0,1] = 25 * um[0] * um[0] * dt * dt * np.cos(xm[2]) * np.sin(xm[2]) * 0.0002
+    Sigma_vv[1,0] = Sigma_vv[0,1]
+
+    Sigma_vv[0,2] = 25 * um[0] * um[0] * dt * dt * 0.0002 / 0.8 * np.tan(um[1]) * np.cos(xm[2])
+    Sigma_vv[2,0] = Sigma_vv[0,2]
+
+    Sigma_vv[1,2] = 25 * um[0] * um[0] * dt * dt * 0.0002 / 0.8 * np.tan(um[1]) * np.sin(xm[2])
+    Sigma_vv[2,1] = Sigma_vv[1,2]
+    #Sigma_vv = np.array([
+    #    [ 0.01,  0.0, 0.0],
+    #    [ 0.0,  0.01 ,  0.0],
+    #    [ 0.0,  0.0,  0.001]
+    #])
 
     mean_ww = np.array([[0.0028458850141975336],
                         [0.041453958735999615]])
     Sigma_ww = np.array([[1.0893397308015538,  0.0],
                          [0.0, 2.9879548591140996]])
-    mean_ww = np.array([[0.0],
-                        [0.0]])
-    Sigma_ww = np.array([
-        [2.24469592, 2.03553876],
-        [2.03553876, 4.55707782]
-    ])
+    #Sigma_ww = np.array([
+    #    [2.24469592, 2.03553876],
+    #    [2.03553876, 4.55707782]
+    #])
 
     ###### Prior update ######
     sm = get_sig(xm, Pm) # get 2n sigma points
@@ -95,11 +112,13 @@ def estRun(time, dt, internalStateIn, steeringAngle, pedalSpeed, measurement):
     xp_hat_k = np.mean(sp, axis=1, keepdims=True)
     Pp_k = (sp - xp_hat_k) @ (sp - xp_hat_k).T / sp.shape[1] + Sigma_vv
 
-    if not isPD(Pp_k):
-        print("Pp_k is not PD")
-
     ###### Posterior update ######
     sp = get_sig(xp_hat_k.flatten(), Pp_k)
+
+    Sigma_ww[0, 0] += 0.5 * 0.5 * np.cos(xp_hat_k[2]) * np.cos(xp_hat_k[2]) *  0.00213
+    Sigma_ww[1, 1] += 0.5 * 0.5 * np.sin(xp_hat_k[2]) * np.sin(xp_hat_k[2]) *  0.00213
+    Sigma_ww[0, 1] = 0.5 * 0.5 * np.sin(xp_hat_k[2]) * np.cos(xp_hat_k[2]) *  0.00213
+    Sigma_ww[1, 0] = Sigma_ww[0, 1]
 
     sz_k = h(sp)[:,0,:] + mean_ww # compute sigma points for the measurements
     z_hat_k = np.mean(sz_k, axis=1, keepdims=True) # expected measurement
@@ -115,16 +134,14 @@ def estRun(time, dt, internalStateIn, steeringAngle, pedalSpeed, measurement):
         # Update cumulative covariances and counts for measurement noise
         cumulative_ww += (z[:, None] - z_hat_k) @ (z[:, None] - z_hat_k).T
         count_ww += 1
+        Pm_k = Pp_k - K_k @ Pzz_k @ K_k.T
     else:
         xm_k = xp_hat_k
+        Pm_k = Pp_k
 
     # Update cumulative covariances and counts for process noise
     cumulative_vv += (xp_hat_k - q(xm_k, um, dt)) @ (xp_hat_k - q(xm_k, um, dt)).T
     count_vv += 1
-
-    Pm_k = Pp_k - K_k @ Pzz_k @ K_k.T
-    if not isPD(Pm_k):
-        print("Pm_k is not PD")
 
     #### OUTPUTS ####
     x, y, theta = xm_k.flatten()
